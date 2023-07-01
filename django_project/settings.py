83c239f50016d3991df361ce4da1eaabbc9baf3d
Khon <99405055+Khon7@users.noreply.github.com>
2023-06-30 23:27:43 -0400

    settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
     'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
